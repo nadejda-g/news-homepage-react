2e077c3fb07a6a72cfbd59837722b90a1fa91723
nadejda-g <ndgolovaciuc@gmail.com>
2025-08-03 11:44:03 +0300

    changed text spacing in main article

--- lib/components/NewArticle.jsx
+++ lib/components/NewArticle.jsx
@@ -2,9 +2,9 @@ import styles from '@/app/page.module.css'
 
 export default function NewArticle({title, description}) {
     return (
-        <>
+        <div className={styles.newArticleCard}>
             <p>{title}</p>
             <p>{description}</p>
-        </>
+        </div>
     )
 }
